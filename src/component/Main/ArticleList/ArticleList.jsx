60aa05605c0bb20869999695bef845dcc3a050a2
duizhang <duizhang627@163.com>
2022-12-19 22:19:03 +0800

    文章列表-无限滚动以及文字超出隐藏

--- src/component/Main/ArticleList/ArticleList.jsx
+++ src/component/Main/ArticleList/ArticleList.jsx
@@ -1,8 +1,12 @@
 import { LikeOutlined, MessageOutlined, StarOutlined } from '@ant-design/icons';
-import { Avatar, List, Space } from 'antd';
-import React from 'react';
+import { Avatar, List, Space,Divider,Skeleton } from 'antd';
+import React, { useState, useEffect } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import './ArticleList.css'
+
+
 const data = Array.from({
-  length: 23,
+  length: 4,
 }).map((_, i) => ({
   href: 'https://ant.design',
   title: `ant design part ${i}`,
@@ -19,46 +23,81 @@ const IconText = ({ icon, text }) => (
   </Space>
 );
 export default function ArticleList() {
+
+
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+  const loadMoreData = () => {
+    if (loading) {
+      return;
+    }
+    setLoading(true);
+    setTimeout(() => {
+      setData([...data, ...Array.from({
+        length: 5,
+      }).map((_, i) => ({
+        href: 'https://ant.design',
+        title: `ant design part ${i}`,
+        avatar: 'https://joeschmoe.io/api/v1/random',
+        description:
+          'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+        content:
+          'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png'
+      }))]);
+      setLoading(false);
+    }, 500)
+  };
+  useEffect(() => {
+    loadMoreData();
+  }, []);
+
   return (
-    <List
-      itemLayout="vertical"
-      size="large"
-      pagination={{
-        onChange: (page) => {
-          console.log(page);
-        },
-        pageSize: 3,
-      }}
-      dataSource={data}
-      footer={
-        <div>
-          <b>ant design</b> footer part
-        </div>
+    <InfiniteScroll
+      dataLength={data.length}
+      next={loadMoreData}
+      hasMore={true}
+      loader={
+        <Skeleton
+          avatar
+          paragraph={{
+            rows: 3,
+          }}
+          active
+        />
       }
-      renderItem={(item) => (
-        <List.Item
-          key={item.title}
-          actions={[
-            <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
-            <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
-            <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
-          ]}
-          extra={
-            <img
-              width={272}
+      endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+    >
+      <List
+        itemLayout="vertical"
+        size="large"
+        dataSource={data}
+        renderItem={(item) => (
+          <List.Item
+            key={item.title}
+            actions={[
+              <IconText icon={StarOutlined} text="156" key="list-vertical-star-o" />,
+              <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o" />,
+              <IconText icon={MessageOutlined} text="2" key="list-vertical-message" />,
+            ]}
+            extra={
+              item.icon != null ? <img
+              width={220}
               alt="logo"
-              src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              src={item.icon}
+            /> : ''
+            }
+            className='list-set'
+          >
+            <List.Item.Meta
+              title={<a href={item.href}>{item.title}</a>}
+              description={item.description}
             />
-          }
-        >
-          <List.Item.Meta
-            avatar={<Avatar src={item.avatar} />}
-            title={<a href={item.href}>{item.title}</a>}
-            description={item.description}
-          />
-          {item.content}
-        </List.Item>
-      )}
-    />
+            <div className='textover-hidden'>{item.content}</div>
+          </List.Item>
+        )
+        }
+      />
+    </InfiniteScroll>
   )
 }

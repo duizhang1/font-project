8d97047d29c3e9d57227608487e3c8540f870899
duizhang <duizhang627@163.com>
2023-02-24 01:49:13 +0800

    完善点赞消息的后台联调，调整数据

--- src/container/NotificationPage/NotificationLike/NotificationLike.jsx
+++ src/container/NotificationPage/NotificationLike/NotificationLike.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
 import NotificationLikeItem from '@src/component/ListItem/NotificationLikeItem/NotificationLikeItem'
-import { Pagination } from 'antd'
+import { message, Pagination, Empty } from 'antd'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { axiosReq } from '@src/util/request/axios'
 
 export default function NotificationLike() {
+
+    const [current, setCurrent] = useState(1)
+    const [total, setTotal] = useState(0)
+    const pageSize = 20
+    const [data, setData] = useState([])
+
+    const pageChange = (page, pageSize) => {
+        setCurrent(page)
+    }
+
+    useEffect(() => {
+        axiosReq.get('/articleLike/getArticleLikeNotifications',
+            { size: pageSize, current: current }).then(
+                (value) => {
+                    setData(value.data.data)
+                    setTotal(value.data.total)
+                },
+                (reason) => {
+                    message.error(reason.message)
+                }
+            )
+    }, [current])
+
     return (
         <div>
-            <NotificationLikeItem />
-            <NotificationLikeItem />
-            <NotificationLikeItem />
+            {data.map((item) => {
+                return <NotificationLikeItem data={item} key={item.relation.uuid} />
+            })}
+            {total === 0 ?
+                (<div style={{
+                    width: '700px',
+                    backgroundColor: '#fff',
+                    margin: '10px 0 0 50px'
+                }}>
+                    <Empty description='暂无点赞消息'/>
+                </div>) : ''
+            }
             <div
                 style={{
                     backgroundColor: '#fff',
@@ -15,7 +50,7 @@ export default function NotificationLike() {
                     margin: '10px 0 0 50px',
                 }}
             >
-                <Pagination defaultCurrent={1} total={50} />
+                <Pagination current={current} total={total} pageSize={pageSize} onChange={pageChange} />
             </div>
         </div>
     )

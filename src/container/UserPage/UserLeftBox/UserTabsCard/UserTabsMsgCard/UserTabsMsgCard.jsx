406a6eff3d7873c0bbc974d31f57a6d44b3a4969
duizhang <duizhang627@163.com>
2023-03-22 00:29:50 +0800

    完成用户动态页的前后端联调

--- src/container/UserPage/UserLeftBox/UserTabsCard/UserTabsMsgCard/UserTabsMsgCard.jsx
+++ src/container/UserPage/UserLeftBox/UserTabsCard/UserTabsMsgCard/UserTabsMsgCard.jsx
@@ -1,60 +1,23 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Avatar, Divider, Skeleton } from 'antd'
 import UserNameHref from '@src/component/AHref/UserNameHref/UserNameHref'
 import InfiniteScroll from 'react-infinite-scroll-component'
 import { axiosReq } from '@src/util/request/axios'
+import { useParams } from 'react-router-dom'
 
 export default function UserTabsMsgCard () {
   const avatarAndNameSpace = '10px'
 
-  const data = [
-    {
-      type: 'focus',
-      itemData: {
-        recordId: '999999999',
-        avatar: 'https://p3-passport.byteimg.com/img/mosaic-legacy/3795/3033762272~180x180.awebp',
-        name: '大队长',
-        userId: '123456',
-        focusName: '小队长',
-        focusUserId: '44444'
-      }
-    },
-    {
-      type: 'likeArticle',
-      itemData: {
-        recordId: '484812662',
-        avatar: 'https://p3-passport.byteimg.com/img/mosaic-legacy/3795/3033762272~180x180.awebp',
-        name: '大队长',
-        userId: '9999999484',
-        companyName: '京东',
-        position: 'software enginee',
-        createTime: '2022-06-06 23:58',
-        title: 'list怎么使用啊啊sadsadddddddddddddddddasdwqrtgsvcfgsdfgfdggrte我红红火火恍恍惚惚水水水水水',
-        summary: '前沿 最近 web3.0 的呼1111111111111111声真的是越来越高前沿 最近 web3.0 的呼1111111111111111声真的是越来越高前沿 最近 web3.0 的呼1111111111111111声真的是越来越高前沿 最近 web3.0 的呼1111111111111111声真的是越来越高，也越来越疯狂。对于我们前端来说，我们需要具备什么技术呢？ 首先先介绍一下 web3.0 是如何衍生的 互联网 我们先聊一下啥...',
-        img: 'http://resource.duizhangz.cn//FqhrHawMGXoRRBWx-NKMp4wqfOT4'
-      }
-    },
-    {
-      type: 'createArticle',
-      itemData: {
-        recordId: '484111812662',
-        avatar: 'https://p3-passport.byteimg.com/img/mosaic-legacy/3795/3033762272~180x180.awebp',
-        name: '大队长',
-        userId: '9999999484',
-        companyName: '京东',
-        position: 'software enginee',
-        createTime: '2022-06-06 23:58',
-        title: 'list怎么使用啊啊sadsadddddddddddddddddasdwqrtgsvcfgsdfgfdggrte我红红火火恍恍惚惚水水水水水',
-        summary: '前沿 最近 web3.0 的呼1111111111111111声真的是越来越高前沿 最近 web3.0 的呼1111111111111111声真的是越来越高前沿 最近 web3.0 的呼1111111111111111声真的是越来越高前沿 最近 web3.0 的呼1111111111111111声真的是越来越高，也越来越疯狂。对于我们前端来说，我们需要具备什么技术呢？ 首先先介绍一下 web3.0 是如何衍生的 互联网 我们先聊一下啥...',
-        img: 'http://resource.duizhangz.cn//FqhrHawMGXoRRBWx-NKMp4wqfOT4'
-      }
-    }
-  ]
+  useEffect(() => {
+    loadMoreData([], 1)
+  }, [])
 
   const itemCss = {
     padding: '10px 0 10px 0',
     borderBottom: '1px solid #e5e6eb'
   }
+
+  const { userId } = useParams()
 
   const [datas, setDatas] = useState([])
   const [currentPage, setCurrentPage] = useState(1)
@@ -64,8 +27,8 @@ export default function UserTabsMsgCard () {
   const loadMoreData = (resetData, resetPage) => {
     const page = resetPage || currentPage
     const data = resetData || datas
-    const params = { current: page, size }
-    axiosReq.get('/article/getArticleList', params).then(
+    const params = { current: page, size, userId }
+    axiosReq.get('/user/getUserAction', params).then(
       (value) => {
         if (!value.data || value.data.length < size) {
           setHasMore(false)
@@ -83,18 +46,18 @@ export default function UserTabsMsgCard () {
 
   function focusItem (item) {
     return (
-      <div style={itemCss} key={item.recordId}>
+      <div style={itemCss} key={item.uuid}>
         <Avatar size={'large'} src={item.avatar} />
         <span style={{
           textAlign: 'center',
           lineHeight: '20px',
           padding: `0 0 0 ${avatarAndNameSpace}`
         }}>
-          <UserNameHref name={item.name} uuid={item.userId} />
+          <UserNameHref name={item.username} uuid={item.userId} />
           <span style={{
             padding: '0 5px'
           }}>关注了</span>
-          <UserNameHref name={item.focusName} uuid={item.focusUserId} />
+          <UserNameHref name={item.beSubscribeName} uuid={item.beSubscribeId} />
         </span>
       </div>
     )
@@ -102,7 +65,7 @@ export default function UserTabsMsgCard () {
 
   function likeArticleItem (item, type) {
     return (
-      <div style={itemCss} key={item.recordId}>
+      <div style={itemCss} key={item.uuid}>
         <div style={{
           display: 'flex'
         }}>
@@ -110,7 +73,7 @@ export default function UserTabsMsgCard () {
           <div style={{
             padding: `0 0 0 ${avatarAndNameSpace}`
           }}>
-            <UserNameHref name={item.name} uuid={item.userId} />
+            <UserNameHref name={item.username} uuid={item.userId} />
             <div>
               {item.position ? item.position + ' @ ' : ''}
               {item.companyName ? item.companyName + ' · ' : ''}
@@ -131,7 +94,7 @@ export default function UserTabsMsgCard () {
           </div>
         </div>
         <a
-          href={`/home/post/${item.recordId}`}
+          href={`/home/post/${item.articleId}`}
           style={{
             margin: '5px 0 5px 50px',
             display: 'block'
@@ -164,16 +127,20 @@ export default function UserTabsMsgCard () {
             >
               {item.summary}
             </div>
-            <img
-              style={{
-                height: '65px',
-                width: '65px',
-                objectFit: 'cover',
-                display: item.img ? 'block' : 'none'
-              }}
-              alt="img"
-              src="http://resource.duizhangz.cn//FqhrHawMGXoRRBWx-NKMp4wqfOT4"
-            />
+            <div style={{
+              margin: '0 0 0 auto'
+            }}>
+              <img
+                style={{
+                  height: '65px',
+                  width: '65px',
+                  objectFit: 'cover',
+                  display: item.img ? 'block' : 'none'
+                }}
+                alt="img"
+                src={item.img}
+              />
+            </div>
           </div>
         </a>
       </div >
@@ -182,7 +149,7 @@ export default function UserTabsMsgCard () {
 
   return (
     <InfiniteScroll
-      dataLength={data.length}
+      dataLength={datas.length}
       next={loadMoreData}
       hasMore={hasMore}
       loader={
@@ -199,11 +166,11 @@ export default function UserTabsMsgCard () {
       pullDownToRefreshThreshold={0}
       endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
     >
-      {data.map((item) => {
+      {datas.map((item) => {
         if (item.type === 'focus') {
-          return focusItem(item.itemData)
+          return focusItem(item.obj)
         } else if (item.type === 'likeArticle' || item.type === 'createArticle') {
-          return likeArticleItem(item.itemData, item.type)
+          return likeArticleItem(item.obj, item.type)
         }
         return ''
       })}
